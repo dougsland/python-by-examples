f632a73c0b15b2f63d08654b873b95b1ee9c8a6a
Douglas Schilling Landgraf <dougsland@redhat.com>
2011-11-04 10:30:18 -0200

    adding checkUserRoot.py

---
+++ checkUserRoot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2011
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import os
+
+def checkRoot():
+	if os.geteuid() != 0:
+		print "You must be root to run this script."
+	else:
+		print "OMG..You are root!"
+
+if __name__ == "__main__":
+	checkRoot()
