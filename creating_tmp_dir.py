ab5655046089368ac54d3c10adc06e9f5f2ebd1b
Douglas Schilling Landgraf <dougsland@gmail.com>
2012-02-09 18:28:24 -0200

    adding creating_tmp_dir.py

---
+++ creating_tmp_dir.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2012
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import tempfile
+import os
+
+try:
+    tmpDir = tempfile.mkdtemp()
+    print "Creating directory: %s" % tmpDir
+finally:
+    print "Removing directory %s" % tmpDir
+    os.removedirs(tmpDir)
