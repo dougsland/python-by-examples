a1f979e88f2a42e3f106eecb12b8c5aedb7509b9
Douglas Schilling Landgraf <dougsland@gmail.com>
2013-12-22 20:06:39 -0500

    More few examples

---
+++ lower.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2013
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+strexample = "TEST TEST TEST"
+
+print "original: {0}\nnew: {1}".format(strexample, strexample.lower())
