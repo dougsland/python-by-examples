8cb3d9a907872635aa9e415be418cd2822f3c564
Douglas Schilling Landgraf <dougsland@gmail.com>
2014-05-13 23:57:49 -0400

    list examples

---
+++ lists/nested.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import time
+
+list = [
+        [1,2,3],
+        [4,5,6]
+       ]
+
+
+print list
+
+print list[0][1]
---
+++ lists/pop.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
+list = ['dog', 'cat']
+
+list.append('cow')
+print(list)
+
+list.pop()
+print(list)
