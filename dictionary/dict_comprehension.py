e22797bd1c2ba9fd2c77f80b50889040eb47fef6
Douglas Schilling Landgraf <dougsland@redhat.com>
2015-07-17 20:35:10 -0300

    comprehension examples

---
+++ dictionary/dict_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2011-2015
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
+mylist = {'2', '4', '6'}
+
+nlist = { int(x) * 2 for x in mylist }
+print(sorted(nlist))
